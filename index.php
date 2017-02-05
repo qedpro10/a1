a5f3f1ffbaf1640bf6ba8a69b8c1fbb9f87f708c
Jens Smith <qedpro@hotmail.com>
2017-02-05 09:41:22 -0800

    prototype of reading quotes from csv file

--- index.php
+++ index.php
@@ -10,12 +10,19 @@
 <body>
    <?php
       $index = rand(0, 3);
-      $test = array("quote1 let the revolution begin", "quote2-let it rain", "quote3 motherfucker", "quote4 die you shitstain");
-      echo $test[$index];
-      ?>
-  <p><?php echo "To announce that there must be no criticism of the president,"; ?></p>
-  <p><?php echo "or that we are to stand by the president right or wrong, is not only unpatriotic and servile,"; ?></p>
-  <p><?php  echo "but is morally treasonable to the American public."; ?></p>
-  <p><?php echo "- Theodore Roosevelt"; ?></p>
+      $row = 1;
+      if (($file = fopen('includes/quotes.csv', "r")) !== FALSE) {
+         // parse the contents of the file into an Array
+
+         while (($quote = fgetcsv($file, 0, ",")) !== FALSE) {
+            $num = count($quote);
+            $row++;
+            for ($c=0; $c < $num; $c++) {
+               echo $quote[$c] . "<br />\n";
+            }
+         }
+      }
+      fclose($file);
+   ?>
 </body>
 </html>
