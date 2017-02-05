f35569464943e32d53d86ea0f4bd0e22d1985fb8
Jens Smith <qedpro@hotmail.com>
2017-02-04 20:32:39 -0800

    initial commit of assignment 1 files

---
+++ includes/htmlhead.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="My Page">
+<meta name="author" content="Jennifer Smith">
+<link rel="icon" type="image/png" href="images/favicon.ico">
+<link rel="stylesheet" href="css/myPage.css">
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Page</title>
+    <?php include('includes/htmlhead.php'); ?>
+</head>
+<header>
+
+</header>
+<body>
+  <p><?php echo "To announce that there must be no criticism of the president,"; ?></p>
+  <p><?php echo "or that we are to stand by the president right or wrong, is not only unpatriotic and servile,"; ?></p>
+  <p><?php  echo "but is morally treasonable to the American public."; ?></p>
+  <p><?php echo "- Theodore Roosevelt"; ?></p>
+</body>
+</html>
