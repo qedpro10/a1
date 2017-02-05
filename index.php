7e17fb32919dd65074ab2935422c923ad73329b0
Jens Smith <qedpro@hotmail.com>
2017-02-05 08:34:24 -0800

    got a test of random number index generation working

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 
 </header>
 <body>
+   <?php
+      $index = rand(0, 3);
+      $test = array("quote1 let the revolution begin", "quote2-let it rain", "quote3 motherfucker", "quote4 die you shitstain");
+      echo $test[$index];
+      ?>
   <p><?php echo "To announce that there must be no criticism of the president,"; ?></p>
   <p><?php echo "or that we are to stand by the president right or wrong, is not only unpatriotic and servile,"; ?></p>
   <p><?php  echo "but is morally treasonable to the American public."; ?></p>
