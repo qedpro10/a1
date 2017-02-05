bcc0cc5301d5e1e0a61df9db0e5fcc04f288c47e
Jens Smith <qedpro@hotmail.com>
2017-02-05 12:50:54 -0800

    basic page layout done

--- includes/htmlhead.php
+++ includes/htmlhead.php
@@ -3,4 +3,5 @@
 <meta name="description" content="My Page">
 <meta name="author" content="Jennifer Smith">
 <link rel="icon" type="image/png" href="images/favicon.ico">
+<link href='//fonts.googleapis.com/css?family=Atma' rel='stylesheet'>
 <link rel="stylesheet" href="css/myPage.css">
--- index.php
+++ index.php
@@ -4,10 +4,18 @@
     <title>My Page</title>
     <?php include('includes/htmlhead.php'); ?>
 </head>
-<header>
 
-</header>
 <body>
+   <header class="center">
+      <img src="images/jas.jpg" width="400" alt="JAS in 1965"/>
+   </header>
+   <div>
+      <p>A little about me.  My name is Jen Smith.  I am the most sarcastic person you'll ever meet.
+         I like getting my hands dirty.  It appears that I always have.  
+         I'm house trained, love car rides but I don't walk well on a leash. I growl at mean people and I will bite assholes.
+         Most people don't get me.  Are you one of them?  Too bad - I don't care.</p>
+   </div>
+   <div class="quote">
    <?php
       if (($file = fopen('includes/quotes.csv', "r")) !== FALSE) {
          // parse the contents of the file into a 2D Array
@@ -17,11 +25,10 @@
       }
       fclose($file);
       $numQuotes = sizeof($quotes);
-      echo "numQuotes = " . $numQuotes;
       $index = rand(0, $numQuotes-1);
-      echo "index = " . $index;
    ?>
    <p> <?php echo $quotes[$index][1] ?> </p>
-   <p> <?php echo "-" . $quotes[$index][0] ?> </p>
+   <p> <?php echo "- " . $quotes[$index][0] ?> </p>
+</div>
 </body>
 </html>
